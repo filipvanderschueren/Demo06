07ca7b827b7fdec9be7f7e34e44ea67703559a1d
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-03-02 20:59:46 +0100

    added a Class & changed main method again

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! ");
+        System.out.println("Hello World!!!! ");
         SomeClass s = new SomeClass();
         s.setMessage( "newMessage" );
         System.out.println("Message from the ClassInstance : "+s.getMessage());
