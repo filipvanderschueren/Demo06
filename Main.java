2c7a93706a3a488a01578bb2811138aa3ae8f356
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-03-02 20:56:47 +0100

    added a Class & changed main method

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! :::");
+        System.out.println("Hello World! ");
+        SomeClass s = new SomeClass();
+        s.setMessage( "newMessage" );
+        System.out.println("Message from the ClassInstance : "+s.getMessage());
     }
 }
---
+++ SomeClass.java
@@ -0,0 +1,11 @@
+public class SomeClass {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
